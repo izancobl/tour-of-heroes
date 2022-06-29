0bb475124bcdb5aef82647a08c529458058e49cc
Izan Cordobilla Blanco <izan.cordobilla@mango.com>
2022-06-29 17:31:24 +0200

    Punto 3 finalizado

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -15,6 +15,11 @@ export class HeroesComponent implements OnInit {
     name: 'WindStorm'
   };
 
+  selectedHero?: Hero;
+  onSelect(hero: Hero): void {
+    this.selectedHero = hero;
+  }
+
   constructor() { }
 
   ngOnInit(): void {
